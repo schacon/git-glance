2482c983014b07e156046c6a9cd7c33e7196a458
Scott Chacon <schacon@gmail.com>
2024-06-30 06:31:28 -0700

    get the first and last

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@
 // - compose release notes and output markdown
 //   - optionally with debug information (which commits/pr)
 
+use anyhow::bail;
 use clap::Parser;
 use git2::Repository;
 
@@ -23,12 +24,31 @@ struct Cli {
     last: Option<String>,
 }
 
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
-    dbg!(cli);
+    dbg!(&cli);
 
     println!("Let's get documenting!");
 
     let repo = Repository::open_from_env().unwrap();
     dbg!(repo.path());
+
+    // get the commit list
+
+    // first, get the tip of the branch (or the -r release sha specified)
+    let tip = match cli.release {
+        Some(release) => repo.revparse_single(&release).unwrap(),
+        None => repo.revparse_single("HEAD").unwrap(),
+    };
+    dbg!(&tip);
+
+    // then, get the last commit (-l last sha specified or last tag)
+    let last = match (cli.last, repo.tag_names(None)?.iter().last()) {
+        (Some(sha), _) => repo.revparse_single(&sha).unwrap(),
+        (_, Some(Some(last_tag))) => repo.revparse_single(last_tag).unwrap(),
+        (_, _) => bail!("no tags found and no last release specified"),
+    };
+    dbg!(&last);
+
+    Ok(())
 }
